065a924d9a9468ca422e25780a76299856716c76
Jackbard <fcktardjoe@gmail.com>
2021-08-09 15:58:47 +0800

    Added PlayerCharacter tests

--- PluralsightxUnitCourse.Tests/PlayerCharacterShould.cs
+++ PluralsightxUnitCourse.Tests/PlayerCharacterShould.cs
@@ -9,6 +9,37 @@ namespace PluralsightxUnitCourse.Tests
         public void BeInexperiencedWhenNew()
         {
             PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.True(sut.IsNoob);
+        }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "Sarah";
+            sut.LastName = "Smith";
+
+            Assert.Equal("Sarah Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.Sleep();
+
+            Assert.InRange<int>(sut.Health, 101, 200);
         }
     }
 }
