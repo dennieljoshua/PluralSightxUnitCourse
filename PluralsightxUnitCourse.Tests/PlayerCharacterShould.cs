783d3e93e1247af7a950667243d5ef4a9bb2c2ca
Jackbard <fcktardjoe@gmail.com>
2021-08-09 16:31:05 +0800

    Added more PlayerCharacter tests

--- PluralsightxUnitCourse.Tests/PlayerCharacterShould.cs
+++ PluralsightxUnitCourse.Tests/PlayerCharacterShould.cs
@@ -41,5 +41,52 @@ namespace PluralsightxUnitCourse.Tests
 
             Assert.InRange<int>(sut.Health, 101, 200);
         }
+
+        [Fact]
+        public void NotHaveNickNameByDefault()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Null(sut.Nickname);
+        }
+
+        [Fact]
+        public void HaveALongBow()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Contains("Long Bow", sut.Weapons);
+        }
+
+        [Fact]
+        public void NotHaveAStaffOfWonder()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.DoesNotContain("Staff of Wonder", sut.Weapons);
+        }
+
+        [Fact]
+        public void HaveAtLeastOneKindOfSword()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Contains(sut.Weapons, weapon => weapon.Contains("Sword"));
+        }
+
+        [Fact]
+        public void HaveAllExpectedWeapons()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            var expectedWeapons = new[]
+            {
+                "Long Bow",
+                "Short Bow",
+                "Short Sword",
+            };
+
+            Assert.Equal(expectedWeapons, sut.Weapons);
+        }
     }
 }
