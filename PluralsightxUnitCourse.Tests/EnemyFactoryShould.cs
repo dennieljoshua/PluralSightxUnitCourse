f92498a103632cee36799f3a8f30090c3a405751
Jackbard <fcktardjoe@gmail.com>
2021-08-09 17:06:39 +0800

    Added EnemyFactory tests

---
+++ PluralsightxUnitCourse.Tests/EnemyFactoryShould.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+using System;
+
+namespace PluralsightxUnitCourse.Tests
+{
+    public class EnemyFactoryShould
+    {
+        [Fact]
+        public void CreateNormalEnemyByDefault()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie");
+
+            Assert.IsType<NormalEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateBossEnemy()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy = sut.Create("Zombie King", true);
+
+            Assert.IsType<BossEnemy>(enemy);
+        }
+
+        [Fact]
+        public void CreateSeparateInstances()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Enemy enemy1 = sut.Create("Zombie");
+            Enemy enemy2 = sut.Create("Zombie");
+
+            Assert.NotSame(enemy1, enemy2);
+        }
+
+        [Fact]
+        public void NotAllowNullName()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            Assert.Throws<ArgumentNullException>(() => sut.Create(null));
+        }
+
+        [Fact]
+        public void OnlyAllowKingOrQueenBossEnemies()
+        {
+            EnemyFactory sut = new EnemyFactory();
+
+            EnemyCreationException ex =
+                Assert.Throws<EnemyCreationException>(() => sut.Create("Zombie", true));
+
+            Assert.Equal("Zombie", ex.RequestedEnemyName);
+        }
+    }
+}
